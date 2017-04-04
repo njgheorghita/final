e8e405ffc8a348abed5d28cb527f0f5d8a1111b0
njgheorghita <njgheorghita@email.wm.edu>
2017-04-04 10:52:46 -0600

    Implement session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    @user = User.find_by_email(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id 
+      #flash
+      redirect_to root_path
+    else
+      #flash
+      redirect_to '/login'
+    end  
   end
 
   def destroy
